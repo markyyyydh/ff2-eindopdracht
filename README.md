7673c7d63777990ba47862403f34c4774b05dabb
Jasper Stolwijk <jasperstolwijk@icloud.com>
2020-04-06 18:33:21 +0200

    Finished product

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # ff2-eindopdracht
 Eindopdrachten Frontend Fundamentals
+
+## Uitleg uitwerking opdrachten
+Deze opdrachten heb ik met beide onderdelen uitgevoerd; er staat niet altijd in de opdracht of het een functie moet zijn of het CSS mag zijn. Hierbij daarvoor een uitleg;
+- Function 1: JS Functie 'setBGColor' (Op blok 2 klikken, 1 veranderd van kleur)
+- Functie 2: JS functie 'setRandomStyles' (Op block 1 klikken)
+- Functie 3: CSS (Op block 4) 
+- Functie 4: JS (Op block 3)
+- Functie 5: JS (Block 5)
+- Functie 6: JS (Block 6)
+- Functie 7: JS (Block 7)
--- index.js
+++ index.js
@@ -1,26 +1,146 @@
 const items = [
   {
-    class: "foo",
-    onClick: 'greeting()',
+    class: "one",
+    onClick: `setRandomStyles('.one')`,
     color: "green"
   },
   {
-    class: "bar",
-    onClick: `setColor('.foo')`,
+    class: "two",
+    onClick: `setBGColor('.one')`,
     color: "red"
-  }
+  },
+  {
+    class: "three",
+    onClick: `setMarginAll()`,
+    color: "blue"
+  },
+  {
+    class: "four",
+    onClick: `setColor('.foo')`,
+    color: "gray"
+  },
+  {
+    class: "five",
+    onClick: `Animation('.five')`,
+    color: "gold"
+  },
+  {
+    class: "six",
+    onClick: `rotateDiv('.six')`,
+    color: "orange"
+  },
+  {
+    class: "seven",
+    onClick: `changeColorEverySecond()`,
+    color: "orangered"
+  },
+  {
+    class: "eight",
+    onClick: `refreshWebPage()`,
+    color: "darkgreen"
+  },
+  {
+    class: "nine",
+    onClick: `goToPage()`,
+    color: "palegreen"
+  },
 ]
 const mainHtml = items.reduce(mkHtmlStr, '');
 
-function setColor (query) {
-  document.querySelector(`${query}`).classList.toggle('color');
+// Functionaliteit 1
+function setBGColor(query) {
+  let bgColor = document.querySelector(`${query}`);
+
+  bgColor.style.transition = "2s";
+
+  if (bgColor.style.background === 'red'){
+    bgColor.style.background = "green";
+  } else {
+    bgColor.style.background = "red";
+  }
 }
 
-function greeting () {
-  alert('Hello!');
+// Functionaliteit 2
+function setRandomStyles(query){
+  let querySelector = document.querySelector(`${query}`);
+
+  querySelector.style.transition = "10s";
+
+  if (querySelector.style.opacity === "0"){
+    querySelector.style.opacity = "1";
+  } else {
+    querySelector.style.opacity = "0";
+  }
+
 }
 
-function mkHtmlStr (htmlStr, item) {
+// Functionaliteit 4
+function setMarginAll(){
+  let querySelector = document.querySelectorAll('div');
+
+  querySelector.forEach(div => {
+    if (div.style.margin === "10px"){
+      div.style.margin = "0"
+    } else {
+      div.style.margin = "10px"
+    }
+  });
+}
+
+// Functionaliteit 5
+function Animation(query){
+  let querySelector = document.querySelector(`${query}`);
+
+  querySelector.style.animationName = "animationFunction5";
+  querySelector.style.animationDuration = "4s";
+}
+
+// Functionaliteit 6
+function rotateDiv(query){
+  let querySelector = document.querySelector(`${query}`);
+
+  if (querySelector.style.transform === "rotate(90deg)"){
+    querySelector.style.transform = "rotate(0deg)";
+  } else {
+    querySelector.style.transform = "rotate(90deg)";
+  }
+
+}
+
+
+// Functionaliteit 7
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++ ) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function changeColor(){
+  let div = document.querySelector('.seven');
+  div.style.backgroundColor= getRandomColor();
+}
+
+function changeColorEverySecond(){
+  setInterval(changeColor,1000);
+}
+
+
+// Functionaliteit 8
+function refreshWebPage(){
+  window.location.reload();
+}
+
+// Functionaliteit 9
+function goToPage(){
+  let url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+  window.open(url ,"_blank")
+}
+
+
+function mkHtmlStr(htmlStr, item) {
   const stringToAppend = `<div onclick="${item.onClick}"
                                class="${item.class}"
                                style="background:${item.color};">
