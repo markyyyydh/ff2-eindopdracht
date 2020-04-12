0de876b3e1e27bb5f8f1966a8604434f3d3180c5
Arunesh <hello@arunesh.me>
2020-04-12 02:35:38 +0200

    Handing in end assignment

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# ff2-eindopdracht
-Eindopdrachten Frontend Fundamentals
+Blocks:
+1. Changes color of block 2 on click
+2. Rotates the middle/fifth block with 45 degrees on click
+3. Slowly increases the height and width of the last/ninth block on hover
+4. Changes the grid gap/margin to 10px on click
+5. Rotates the eighth block 360 degrees on click
+6. Rotates the seventh block 90 degrees on click, and back 90 degrees again if it has already been rotated
+7. Changes the block's color every second on click
+8. Reloads the page on click
+9. Opens a YouTube page on click, in a new tab
--- index.js
+++ index.js
@@ -1,32 +1,128 @@
-const items = [
+// Is seventh block rotated already
+let seventhRotated = false;
+
+// Define the blocks that will be rendered
+const blocks = [
   {
-    class: "foo",
-    onClick: 'greeting()',
-    color: "green"
+    color: 'black',
+    onClick: () => {
+      // Grab secondth block and set the colorTransition class on it, with a bg color of purple
+      const secondBlockDiv = document.getElementById('block-2')
+
+      secondBlockDiv.classList.add('colorTransition')
+      secondBlockDiv.style.backgroundColor = 'gray'
+    }
   },
   {
-    class: "bar",
-    onClick: `setColor('.foo')`,
-    color: "red"
+    color: 'crimson',
+    onClick: () => {
+      // Grab middle (fifth) block and rotate it 45 degrees
+      const middleBlockDiv = document.getElementById('block-5')
+
+      middleBlockDiv.classList.add('slightlyRotated')
+    }
+  },
+  {
+    color: 'red',
+    onHover: () => {
+      // Grab last (ninth) block and slowly make it bigger
+      const lastBlockDiv = document.getElementById('block-9')
+
+      lastBlockDiv.classList.add('growingBlock')
+    }
+  },
+  {
+    color: 'springgreen',
+    onClick: () => {
+      // Override margin/gap of the grid to be 10px
+      const blocksContainer = document.getElementById('blocksContainer')
+
+      blocksContainer.style.gridGap = '10px'
+    }
+  },
+  {
+    color: 'lime',
+    onClick: () => {
+      // Grab eighth block and rotate it 360 degrees
+      const eighthBlockDiv = document.getElementById('block-8')
+
+      eighthBlockDiv.classList.add('fullyRotated')
+    }
+  },
+  {
+    color: 'lawngreen',
+    onClick: () => {
+      // Grab seventh block and rotate it 90 degrees right, and back if already rotated
+      const seventhBlockDiv = document.getElementById('block-7')
+
+      if (!seventhRotated) {
+        seventhRotated = true
+
+        seventhBlockDiv.style.transition = 'transform 2s'
+        seventhBlockDiv.style.transform  = 'rotate(90deg)'
+        return
+      }
+
+      // Rotate back, but keep transition style
+      seventhRotated = false
+      seventhBlockDiv.style.removeProperty('transform')
+    }
+  },
+  {
+    color: 'aqua',
+    onClick: () => {
+      // Grab seventh block and change it's color every second
+      const seventhBlockDiv = document.getElementById('block-7')
+
+      setInterval(() => {
+        seventhBlockDiv.style.backgroundColor = `#${Math.random().toString(16).substr(-6)}`
+      }, 1000)
+    }
+  },
+  {
+    color: 'aquamarine',
+    onClick: () => location.reload()
+  },
+  {
+    color: 'cadetblue',
+    onClick: () => window.open('https://www.youtube.com/watch?v=LeMVDuIO3J0', '_blank')
   }
 ]
-const mainHtml = items.reduce(mkHtmlStr, '');
 
-function setColor (query) {
-  document.querySelector(`${query}`).classList.toggle('color');
+// Get blocks container
+const blocksContainer = document.getElementById('blocksContainer')
+
+// Render the blocks
+function renderBlocks () {
+  blocks.forEach((block, index) => {
+    const blockDiv = document.createElement('div')
+
+    // Add block ID to div for future reference
+    blockDiv.id = `block-${index + 1}`
+  
+    // Add the base class
+    blockDiv.classList.add('block')
+  
+    // Add color if the block has that
+    if (block.color) {
+      blockDiv.style.backgroundColor = block.color
+    } 
+    
+    // Add a onclick event listener if block has it
+    if (block.onClick) {
+      blockDiv.style.cursor = 'pointer'
+      blockDiv.onclick      = block.onClick
+    }
+
+    // Add a onhover event listener if block has it
+    if (block.onHover) {
+      blockDiv.onmouseover = block.onHover
+    } 
+  
+    // Add block to the container
+    blocksContainer.appendChild(blockDiv)
+  })
 }
 
-function greeting () {
-  alert('Hello!');
-}
-
-function mkHtmlStr (htmlStr, item) {
-  const stringToAppend = `<div onclick="${item.onClick}"
-                               class="${item.class}"
-                               style="background:${item.color};">
-                          </div>`;
-
-  return htmlStr + stringToAppend;
-}
-
-document.querySelector('main').innerHTML = mainHtml;
+// Initial render
+renderBlocks()
